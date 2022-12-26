36eee897886590be756dbb6f199a5b15d65eb7ae
GutoAndrade01 <gutoandrade01@gmail.com>
2022-12-25 23:47:57 -0300

    teste final

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
-    $servidor = "localhost";
+    $servidor = "us-cdbr-east-06.cleardb.net";
     $port = "3306";
-    $usuario = "root";
-    $password = "";
-    $dbname = "teste";
+    $usuario = "b2f36ade5577ec";
+    $password = "bd4e5172";
+    $dbname = "heroku_7fa5047aa930f7e";
 
     $conexao = mysqli_connect($servidor, $usuario, $password, $dbname);
      if(!$conexao){
